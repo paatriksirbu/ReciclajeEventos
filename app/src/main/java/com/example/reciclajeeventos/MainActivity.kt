182be8376fdd7e1422af6f0119d6514d05f104e3
paatriksirbu <psirb@myuax.com>
2024-10-01 20:45:24 +0200

    Primera fase del MainActivity.kt donde creamos un boton que nos muestra las opciones a elegir.

--- app/src/main/java/com/example/reciclajeeventos/MainActivity.kt
+++ app/src/main/java/com/example/reciclajeeventos/MainActivity.kt
@@ -1,47 +1,98 @@
 package com.example.reciclajeeventos
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
 import com.example.reciclajeeventos.ui.theme.ReciclajeEventosTheme
+import java.security.AccessController
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ReciclajeEventosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                PantallaPrincipal(navController)
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PantallaPrincipal(navController: NavController) {
+
+    var showButtons by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Bienvenido, EcoAmigo!")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //Creamos un boton principal para mostrar las opciones
+
+        if(!showButtons) {
+            Button(
+                onClick = { showButtons = true },
+                modifier = Modifier.padding(vertical = 8.dp).height(50.dp)
+            ) {
+                Text(text = "Mostrar opciones")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        if (showButtons){
+            Button(onClick = {navController.navigate("calendar")}) {
+                Text(text = "Calendario")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {navController.navigate("mapa")}) {
+                Text(text = "Mapa")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {navController.navigate("estadisticas")}) {
+                Text(text = "Estadisticas")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ReciclajeEventosTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        PantallaPrincipal(navController)
+
     }
 }
