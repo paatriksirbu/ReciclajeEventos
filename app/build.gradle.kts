82fa482725f91995627b870aa00c1f9fd37906ea
itsedupablo <pedugar@myuax.com>
2024-10-02 11:52:28 +0200

    Se ha creado la clase MapActivity para más adelante mostrar un mapa de los puntos de recogida de residuos en la app.
    Se ha actualizado build.gradle.kts y libs.versions.toml para inyectar una dependencia que permita meter imágenes (en este caso el "mapa")

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.photoview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -67,5 +67,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 
 }
