e84034ff45c3906db706183ce3213144a894e597
Patrik <psirb@myuax.com>
2024-10-02 10:25:16 +0200

    Creacion del CalendarActivity y su xml

---
+++ app/src/main/java/com/example/reciclajeeventos/CalendarActivity.kt
@@ -0,0 +1,31 @@
+package com.example.reciclajeeventos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+
+class CalendarActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_calendar)
+
+        val composeView = findViewById<ComposeView>(R.id.compose_view)
+    }
+
+    @Composable
+    fun calendario(){
+
+    }
+
+    @Composable
+    @Preview(showBackground = true)
+    fun calendarioPreview(){
+        calendario()
+    }
+
+}
